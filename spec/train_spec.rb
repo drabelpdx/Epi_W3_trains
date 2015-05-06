b2676456b2abac827e6141b0b6c7cd96b917909c
Debbie Balbo & David Abel <student@epicodus.com>
2015-05-06 11:07:04 -0700

    Adds name and id methods to Train class.

---
+++ spec/train_spec.rb
@@ -0,0 +1,18 @@
+require('spec_helper')
+
+describe(Train) do
+
+  describe('#name') do
+    it('returns the name') do
+      train = Train.new({:name => 'Red Line', :id => nil})
+      expect(train.name()).to(eq('Red Line'))
+    end
+  end
+
+  describe('#id') do
+    it('returns the id') do
+    train = Train.new({:name => 'Red Line', :id => 1})
+    expect(train.id()).to(eq(1))
+  end
+end
+end
