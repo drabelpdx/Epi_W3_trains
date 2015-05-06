dec14636749675c95e4c02aa6742198ba610ae88
Debbie Balbo & David Abel <student@epicodus.com>
2015-05-06 16:49:47 -0700

    End of day commit good work.

--- app.rb
+++ app.rb
@@ -17,7 +17,27 @@ get("/trains") do
   erb(:trains)
 end
 
-get("/trains_form") do
+get("/cities") do
+  @cities = City.all()
+  erb(:cities)
+end
+
+get("/trains/:id") do
+  @train =Train.find(params.fetch("id").to_i())
+  @trains = Train.all()
+  erb(:train)
+end
+
+patch("/t_update/:id") do
+  train_id = params.fetch("id").to_i()
+  @train = Train.find(train_id)
+  city_ids = params.fetch("city_ids")
+  @train.update({:city_ids => city_ids})
+  @cities = City.all()
+  erb(:train)
+end
+
+get("/trains/new") do
   erb(:trains_form)
 end
 
@@ -27,4 +47,30 @@ post("/trains") do
   train.save()
   @trains = Train.all()
   erb(:trains)
+end
+
+get("/cities/new") do
+  erb(:cities_form)
+end
+
+post("/cities") do
+  name = params.fetch("name")
+  city = City.new({:name => name, :id => nil})
+  city.save()
+  @cities = City.all()
+  erb(:cities)
+end
+
+get("/cities/:id") do
+  @city = City.find(params.fetch("id").to_i())
+  erb(:city)
+end
+
+patch("/c_update/:id") do
+  city_id = params.fetch("id").to_i()
+  @city = City.find(city_id)
+  train_ids = params.fetch("train_ids")
+  @city.update({:train_ids => train_ids})
+  @trains = Train.all()
+  erb(:city)
 end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,11 +3,20 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exception, false)
 
-describe('the Train path', {:type => :feature}) do
-  it('it creates a list of trains') do
-    visit('/trains_form')
+describe('creates a City and Train path', {:type => :feature}) do
+  it('it creates a list of train routes and cities') do
+    visit('/trains/new')
     fill_in('name', :with => 'Red Line')
     click_button('Add Train')
+    expect(page).to have_content('Red Line')
+  end
+end
+
+describe('update Train path', {:type => :feature}) do
+  it('it updates a train record') do
+    visit('/t_update/:id')
+    fill_in('name', :with => 'Red Line')
+    click_button('Update Train')
     expect(page).to have_content('Red Line')
   end
 end
